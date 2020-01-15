e676632eabee10195b5a9f1321a2e181c0fcb42b
Ragan Martinez <ragan.martinez@live.com>
2020-01-16 10:34:44 +1100

    Setup basic express boilerplate

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const expressSession = require("express-session");
+const MongoStore = require("connect-mongo")(expressSession);
+const mongoose = require("mongoose");
+// const passport = require("./config/passport");
+const cookieParser = require("cookie-parser");
+const app = express();
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 6000000
+    },
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+);
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// app.use(passport.initialize());
+// app.use(passport.session());
+app.use(require("./routes"));
+
+module.exports = app;
---
+++ database/schemas/admin_schema.js
@@ -0,0 +1,19 @@
+const { Schema } = require("mongoose");
+
+const AdminSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    bcrypt: true
+  }
+});
+
+AdminSchema.plugin(require("mongoose-bcrypt"));
+
+module.exports = AdminSchema;
