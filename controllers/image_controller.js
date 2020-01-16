3ccd93bd2a708760e96c8129ac4a6964386d5d8f
Ragan Martinez <ragan.martinez@live.com>
2020-01-17 10:40:13 +1100

    Added image controller methods and routes

--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -1,14 +1,39 @@
-// require model
+const ImageModel = require("./../database/models/image_model");
 
-async function index(req, res) {}
+async function index(req, res) {
+  const images = await ImageModel.find();
+  res.json(images);
+}
 
-async function create(req, res) {}
+async function create(req, res) {
+  const { url, caption } = req.body;
+  const image = await ImageModel.create({ url, caption });
+  res.json(image);
+}
 
-async function update(req, res) {}
+async function update(req, res) {
+  const { id } = req.params;
+  const { caption } = req.body;
+  const image = await ImageModel.findByIdAndUpdate(
+    id,
+    { caption },
+    { new: true },
+  );
+  res.json(image);
+}
 
-async function show(req, res) {}
+async function show(req, res) {
+  const { id } = req.params;
+  console.log(id);
+  const image = await ImageModel.findById(id);
+  res.json(image);
+}
 
-async function destroy(req, res) {}
+async function destroy(req, res) {
+  const { id } = req.params;
+  const image = await ImageModel.findByIdAndRemove(id);
+  res.json(image);
+}
 
 module.exports = {
   index,
--- routes/image_routes.js
+++ routes/image_routes.js
@@ -2,4 +2,24 @@ const express = require("express");
 const router = express.Router();
 const ImageController = require("./../controllers/image_controller");
 
+// GET all images
+
+router.get("/", ImageController.index);
+
+// GET image by id
+
+router.get("/:id", ImageController.show);
+
+// POST image
+
+router.post("/", ImageController.create);
+
+// PUT image
+
+router.put("/:id", ImageController.update);
+
+// DELETE image
+
+router.delete("/:id", ImageController.destroy);
+
 module.exports = router;
