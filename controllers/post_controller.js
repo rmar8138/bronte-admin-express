348109ea119f5dee8c6a40b2e5eb4ed64a0b3f0d
Luke <lukepierson@hotmail.com>
2020-01-16 15:59:18 +1100

    posts controller and routes complete

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,14 +1,40 @@
-// require model
+const PostModel = require("./../database/models/post_model");
 
-async function index(req, res) {}
+async function index(req, res) {
+  const posts = await PostModel.find();
+  res.json(posts);
+}
 
-async function create(req, res) {}
+async function show(req, res) {
+  const { id } = req.params;
+  const post = await PostModel.findById(id);
+  res.json(post);
+}
 
-async function update(req, res) {}
+async function create(req, res) {
+  const { title, body } = req.body;
+  const post = await PostModel.create({ title, body }).catch(err =>
+    res.status(500).send(err),
+  );
+  res.json(post);
+} //need to lock down
 
-async function show(req, res) {}
+async function update(req, res) {
+  const { title, body } = req.body;
+  const { id } = req.params;
+  const post = await PostModel.findByIdAndUpdate(
+    id,
+    { title, body },
+    { new: true },
+  );
+  res.json(post);
+} //need to lock down
 
-async function destroy(req, res) {}
+async function destroy(req, res) {
+  const { id } = req.params;
+  const post = await PostModel.findByIdAndRemove(id);
+  res.json(post);
+} //need to lock down
 
 module.exports = {
   index,
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -2,4 +2,14 @@ const express = require("express");
 const router = express.Router();
 const PostController = require("./../controllers/post_controller");
 
+router.get("/", PostController.index);
+
+router.post("/", PostController.create);
+
+router.get("/:id", PostController.show);
+
+router.put("/:id", PostController.update);
+
+router.delete("/:id", PostController.destroy);
+
 module.exports = router;
