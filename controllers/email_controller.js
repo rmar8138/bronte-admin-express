c41ae5fe71bf60b854ff43621aa04c41eb552ba7
Luke <lukepierson@hotmail.com>
2020-01-20 10:24:27 +1100

    SendGrid up and running yew

--- controllers/email_controller.js
+++ controllers/email_controller.js
@@ -1,4 +1,21 @@
-async function sendEmail(req, res) {}
+const sgMail = require("@sendgrid/mail");
+
+async function sendEmail(req, res) {
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "ragan.martinez@live.com",
+    from: "lukepierson@hotmail.com",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html:
+      "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  const response = await sgMail.send(msg);
+  res.json(response);
+}
 
 module.exports = {
   sendEmail,
--- routes/email_routes.js
+++ routes/email_routes.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const EmailController = require("./../controllers/email_controller");
 
+router.post("/", EmailController.sendEmail);
+
 module.exports = router;
